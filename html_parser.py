4b02edcbc646c089f83fdb230dcb98496651afb1
AnnHo01 <punri1304@gmail.com>
2021-12-12 20:58:44 -0600

    Add html_parser.py to handle scraping functions

---
+++ html_parser.py
@@ -0,0 +1,82 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    """This class perform scraping actions based on given url."""
+    def __init__(self):
+        try:
+            HTMLParser.__init__(self)
+            self.tbody_flag = False
+            self.td_flag = False
+            self.tr_flag = False
+            self.abbr_flag = False
+            self.td_counter = 0
+            self.tr_counter = 0
+            self.daily_temps = {}
+            self.weather ={}
+            self.day = ""
+            self.year = ""
+            self.month = ""
+        except Exception as e:
+            print("WeatherScraper:init", e)
+
+    def handle_starttag(self, tag, attrs):
+        """This functiona handles the starting tags"""
+        try:
+            if tag == "tbody":
+                self.tbody_flag = True
+            if tag == "tr":
+                self.tr_counter += 1
+                self.tr_flag = True
+            if tag == "td":
+                self.td_counter += 1
+                self.td_flag = True
+            if tag == "abbr":
+                self.abbr_flag = True
+                for name, value in attrs:
+                    try:
+                        if name == "title" and self.tr_counter > 1:
+                            self.day = value
+                            if(self.day.find(',') >= 0):
+                                self.year = self.day[self.day.find(',') + 1: ]
+                                self.month = self.day[0:self.day.find(',') - 2]
+                    except Exception as e:
+                        print("WeatherScraper:handle_starttag:loop", e)
+
+        except Exception as error:
+            print("WeatherScraper:handle_starttag", error)
+
+
+    def handle_endtag(self, tag):
+        """This function handles the end tags"""
+        try:
+            if tag == "tbody":
+                self.tbody_flag = False
+                self.tr_counter = 0
+            if tag == "td":
+                self.td_flag = False
+            if tag == "tr":
+                self.tr_flag = False
+                self.td_counter = 0
+                self.daily_temps = {}
+            if tag == "abbr":
+                self.abbr_flag = False
+        except Exception as e:
+            print("WeatherScraper:handle_endtag", e)
+
+
+
+    def handle_data(self, data):
+        """This fuinction handles the data"""
+        try:
+            if self.tbody_flag and self.td_flag and self.tr_flag:
+                self.temp = data
+                if self.td_counter == 1:
+                    self.daily_temps.update({"Max Temp": self.temp})
+                if self.td_counter == 2:
+                    self.daily_temps.update({"Min Temp": self.temp})
+                if self.td_counter == 3:
+                    self.daily_temps.update({"Mean Temp": self.temp})
+                if (self.day != "Average") and (self.day != "Extreme") and (self.day != ''):
+                    self.weather.update({self.day: self.daily_temps})
+        except Exception as e:
+            print("WeatherScraper:handle_data", e)
--- scrape_weather.py
+++ scrape_weather.py
@@ -1,176 +1,85 @@
-from html.parser import HTMLParser
-from re import match
+import html_parser as parser
 import urllib.request
-from decimal import Decimal
 from datetime import date, datetime
 
+
 """This module scrapes the data from the Environment Canada website and prints returns the dictionary of dictionaries of data"""
-
-class WeatherScraper(HTMLParser):
+class WeatherScraper():
     """This class parses the HTML and returns the data"""
-
-    def __init__(self):
+    def early_year(self, type):
+      """This function is finding out the earliest year on the website"""
       try:
-        HTMLParser.__init__(self)
-        self.tbody_flag = False
-        self.td_flag = False
-        self.tr_flag = False
-        self.abbr_flag = False
-        self.td_counter = 0
-        self.tr_counter = 0
-        self.daily_temps = {}
-        self.weather ={}
-        self.day = ""
-        self.year = ""
-        self.month = ""
-      except Exception as e:
-          print("WeatherScraper:init", e)
-
-
-
-
-    def handle_starttag(self, tag, attrs):
-      """This functiona handles the starting tags"""
-      try:
-        if tag == "tbody":
-          self.tbody_flag = True
-        if tag == "tr":
-          self.tr_counter += 1
-          self.tr_flag = True
-        if tag == "td":
-          self.td_counter += 1
-          self.td_flag = True
-        if tag == "abbr":
-          self.abbr_flag = True
-          for name, value in attrs:
-            try:
-              if name == "title" and self.tr_counter > 1:
-                self.day = value
-                if(self.day.find(',') >= 0):
-                  self.year = self.day[self.day.find(',') + 1: ]
-                  self.month = self.day[0:self.day.find(',') - 2]
-            except Exception as e:
-              print("WeatherScraper:handle_starttag:loop", e)
-
-      except Exception as e:
-        print("WeatherScraper:handle_starttag", e)
-
-
-    def handle_endtag(self, tag):
-      """This function handles the end tags"""
-      try:
-        if tag == "tbody":
-          self.tbody_flag = False
-          self.tr_counter = 0
-          # for name, value in self.weather.items():
-          #   print(name, value)
-        if tag == "td":
-              self.td_flag = False
-        if tag == "tr":
-          self.tr_flag = False
-          self.td_counter = 0
-          self.daily_temps = {}
-        if tag == "abbr":
-          self.abbr_flag = False
-      except Exception as e:
-        print("WeatherScraper:handle_endtag", e)
-
-
-
-    def handle_data(self, data):
-      """This fuinction handles the data"""
-      try:
-        if self.tbody_flag and self.td_flag and self.tr_flag:
-          self.temp = data
-          if self.td_counter == 1:
-            self.daily_temps.update({"Max Temp": self.temp})
-          if self.td_counter == 2:
-            self.daily_temps.update({"Min Temp": self.temp})
-          if self.td_counter == 3:
-            self.daily_temps.update({"Mean Temp": self.temp})
-          if (self.day != "Average") and (self.day != "Extreme") and (self.day != ''):
-            self.weather.update({self.day: self.daily_temps})
-      except Exception as e:
-        print("WeatherScraper:handle_data", e)
-
-
-def early_year(type):
-  """This function is finding out the earliest year on the website"""
-  try:
-    today = date.today()
-    input = today.year
-    url = f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear={input}&Day=1&Year=1840'
-    myparser = WeatherScraper()
-    with urllib.request.urlopen(url) as response:
-        html = str(response.read())
-    myparser.feed(html)
-    year = myparser.year
-    month = myparser.month
-    if(type=="y"):
-      return year
-    elif(type=="m"):
-      return month
-  except Exception as e:
-      print("WeatherScraper:early_year", e)
-
-
-def get_weather(latest_year = None, latest_month = None):
-  """This function scrapes the data"""
-  try:
-    result = {}
-    today = date.today()
-    input = today.year
-    new_month = 0
-    early = None
-    eMonth = None
-    if latest_year != None:
-      early = latest_year
-      eMonth = str(latest_month)
-    else:
-      early = early_year("y")
-      eMonth = early_year("m")
-
-    for i in range((int(input) - int(early) + 1)):
-      try:
-        year_to_loop = int(early) + i
-        for j in range(12):
-          month = j + 1
-          myparser = WeatherScraper()
-          with urllib.request.urlopen(f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear={input}&Day=1&Year={year_to_loop}&Month={month}#') as response:
+          today = date.today()
+          input = today.year
+          url = f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear={input}&Day=1&Year=1840'
+          myparser = parser.MyHTMLParser()
+          with urllib.request.urlopen(url) as response:
               html = str(response.read())
-
           myparser.feed(html)
-          if not eMonth.isnumeric():
-              datetime_object = datetime.strptime(eMonth.strip(), "%B")
-              month_num = datetime_object.month
-          else:
-              month_num = int(eMonth)
-
-          for key, value in myparser.weather.items():
-            try:
-              date_format = datetime.strptime(str(key), '%B %d, %Y' )
-              right_format = date_format.strftime('%Y-%m-%d')
-              if year_to_loop == int(early):
-                if month >= int(month_num):
-                  result.update({right_format: value})
-                  if month != new_month:
-                    print("Processing: " + key[:key.find(',') - 2] + f" {year_to_loop}")
-                    new_month = month
-              else:
-                  result.update({right_format: value})
-                  if month != new_month:
-                    print("Processing: " + key[:key.find(',') - 2] + f" {year_to_loop}")
-                    new_month = month
-            except Exception as e:
-              print("WeatherScraper:get_weather:loop_3", e)
+          year = myparser.year
+          month = myparser.month
+          if(type=="y"):
+              return year
+          elif(type=="m"):
+              return month
       except Exception as e:
-          print("WeatherScraper:get_weather:loop_1", e)
-  except Exception as e:
-    print("WeatherScraper:get_weather", e)
+          print("WeatherScraper:early_year", e)
 
 
+    def get_weather(self, latest_year = None, latest_month = None):
+        """This function scrapes the data"""
+        try:
+            result = {}
+            today = date.today()
+            input = today.year
+            new_month = 0
+            early = None
+            eMonth = None
+            if latest_year != None:
+                early = latest_year
+                eMonth = str(latest_month)
+            else:
+                early = self.early_year("y")
+                eMonth = self.early_year("m")
 
+            for i in range((int(input) - int(early) + 1)):
+                try:
+                    year_to_loop = int(early) + i
+                    for j in range(12):
+                        month = j + 1
+                        myparser = WeatherScraper()
+                        with urllib.request.urlopen(f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear={input}&Day=1&Year={year_to_loop}&Month={month}#') as response:
+                            html = str(response.read())
 
-  return result
+                        myparser.feed(html)
+                        if not eMonth.isnumeric():
+                            datetime_object = datetime.strptime(eMonth.strip(), "%B")
+                            month_num = datetime_object.month
+                        else:
+                            month_num = int(eMonth)
+
+                        for key, value in myparser.weather.items():
+                            try:
+                                date_format = datetime.strptime(str(key), '%B %d, %Y' )
+                                right_format = date_format.strftime('%Y-%m-%d')
+                                if year_to_loop == int(early):
+                                    if month >= int(month_num):
+                                        result.update({right_format: value})
+                                        if month != new_month:
+                                            print("Processing: " + key[:key.find(',') - 2] + f" {year_to_loop}")
+                                            new_month = month
+                                else:
+                                    result.update({right_format: value})
+                                    if month != new_month:
+                                        print("Processing: " + key[:key.find(',') - 2] + f" {year_to_loop}")
+                                        new_month = month
+                            except Exception as e:
+                                print("WeatherScraper:get_weather:loop_3", e)
+                except Exception as e:
+                    print("WeatherScraper:get_weather:loop_1", e)
+            return result
+        except Exception as e:
+            print("WeatherScraper:get_weather", e)
+
 
 # print(get_weather())
